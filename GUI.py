67aea03e7e38b89911884a72e68ee80212cbf698
LoicDeroWastaken <l.dero@students.ephec.be>
2023-05-17 01:55:38 +0200

    Code propre et fini, commenté et expliqué

--- GUI.py
+++ GUI.py
@@ -3,15 +3,15 @@ from tkinter import messagebox
 from tkinter import ttk
 import serial
 
-# Cette fonction va nous permettre d'injecter les données dans une variable partagées qui ensuite seront envoyées au pico
 def envois(data):
+    # Fonction pour envoyer les données au périphérique connecté
     ser = serial.Serial(port="COM4", baudrate=9600, timeout=1)
     ser.write(bytes(str(data) + '\r\n', 'utf-8'))
     ser.flush()
     ser.close()
 
-#
 def update_distance_limit():
+    # Fonction pour mettre à jour la limite de distance
     limit = distance_limit_entry.get()
     try:
         limit = int(limit)
@@ -23,64 +23,51 @@ def update_distance_limit():
         messagebox.showerror(
             "Erreur", "Veuillez entrer une valeur numérique entière entre 1 et 250 pour la limite de distance.")
 
-# Cette fonction nous permet de mettre a jour les données qui sont lu sur le pico
 def on_distance_limite_send(event):
+    # Fonction appelée lors de l'envoi de la limite de distance
     on_distance_change(event)
     data = str(int(distance_var.get())) + "," + str(int(limite_var.get()))
     envois(data)
 
-# Cette fonction nous permet d'écrire des données dans le stdin du pico et de lire les données envoyées par le pico
 def on_distance_change(event):
-    distance = int(distance_scale.get())  # Distance entière
-    distance_var.set(distance)  # Mise à jour de la variable de distance
+    # Fonction appelée lors du changement de la distance
+    distance = int(distance_scale.get())
+    distance_var.set(distance)
 
 # Création de la fenêtre principale
 window = tk.Tk()
 window.title("Interface de distance")
-window.minsize(400, 250)
-window.maxsize(400, 250)
+window.geometry("500x400")  # Taille de la fenêtre
 
-# Variable pour stocker la valeur de distance
-distance_var = tk.IntVar(value=1)  # Initialisation à 1 centimetre
-limite_var = tk.IntVar(value=250) # Initialisation à 1 centimetre
+distance_var = tk.IntVar(value=1)  # Variable pour stocker la valeur de distance
+limite_var = tk.IntVar(value=250)  # Variable pour stocker la valeur de limite de distance
 
-# Création des widgets
-title_label = ttk.Label(window, text="Distance :")
-title_label.pack()
+# Création des éléments de l'interface utilisateur
+title_label = ttk.Label(window, text="Distance :", font=("Arial", 12, "bold"))
+title_label.pack(pady=10)
 
-distance_scale = ttk.Scale(window, from_=0, to=250,
-                           length=250, orient=tk.HORIZONTAL)
-distance_scale.pack()
-distance_scale.set(100)  # Initialisation à 100 cm
+distance_scale = ttk.Scale(window, from_=0, to=250, length=250, orient=tk.HORIZONTAL)
+distance_scale.pack(pady=10)
+distance_scale.set(100)
 
-distance_limit_label = ttk.Label(window, text="Limite de distance:")
+distance_limit_label = ttk.Label(window, text="Limite de distance :", font=("Arial", 12, "bold"))
 distance_limit_label.pack()
 
 distance_limit_entry = ttk.Entry(window)
-distance_limit_entry.pack()
+distance_limit_entry.pack(pady=10)
 
-update_button = ttk.Button(
-    window, text="Mettre à jour", command=update_distance_limit)
-update_button.pack()
+update_button = ttk.Button(window, text="Mettre à jour", command=update_distance_limit)
+update_button.pack(pady=10)
 
-distance_gauge = ttk.Progressbar(
-    window, variable=distance_var, mode="determinate", maximum=250)
-distance_gauge.pack()
-distance_var.set(100)  # Initialisation à 1 mètre
-
-ttk.Label(window, text="Distance :").pack()
-distance_text_label = ttk.Label(
-    window, textvariable=distance_var, font=("Arial", 14, "bold"))
+ttk.Label(window, text="Distance :", font=("Arial", 12, "bold")).pack()
+distance_text_label = ttk.Label(window, textvariable=distance_var, font=("Arial", 14, "bold"))
 distance_text_label.pack()
 
-ttk.Label(window, text="Limite à ne pas dépasser :").pack()
-distance_text_label = ttk.Label(
-    window, textvariable=limite_var, font=("Arial", 14, "bold"))
+ttk.Label(window, text="Limite à ne pas dépasser :", font=("Arial", 12, "bold")).pack()
+distance_text_label = ttk.Label(window, textvariable=limite_var, font=("Arial", 14, "bold"))
 distance_text_label.pack()
 
-# Appel de la fonction on_distance_change lorsque la jauge est déplacée
 distance_scale.bind("<B1-Motion>", on_distance_change)
 distance_scale.bind("<ButtonRelease-1>", on_distance_limite_send)
 
-# Exécution de la boucle principale
 window.mainloop()
--- Rasberry.py
+++ Rasberry.py
@@ -1,9 +1,7 @@
 import machine
 import utime
-import time
 import sys
 import select
-
 
 poller = select.poll()
 poller.register(sys.stdin, 1)
@@ -21,11 +19,10 @@ def read_from_port():
 
 # Fonction pour mesurer la distance avec la sonde HC-SR04
 def dechiffre():
-    global distance
-    global alarm
+    global distance, alarm
   
     data = read_from_port()
-    if data != None:
+    if data is not None:
         data_Array = data.split(',')
         distance = int(data_Array[0])
         alarm = int(data_Array[1])
@@ -49,33 +46,27 @@ digit_patterns = {
 }
 
 # Définition des broches pour les deux afficheurs 7 segments
-afficheur_Dixaines = []
-afficheur_Unitees = []
+afficheur_Dixaines = [
+    machine.Pin(8, machine.Pin.OUT),
+    machine.Pin(9, machine.Pin.OUT),
+    machine.Pin(10, machine.Pin.OUT),
+    machine.Pin(11, machine.Pin.OUT),
+    machine.Pin(12, machine.Pin.OUT),
+    machine.Pin(13, machine.Pin.OUT),
+    machine.Pin(14, machine.Pin.OUT)
+]
 
-segment_0 = machine.Pin(8, machine.Pin.OUT)
-segment_1 = machine.Pin(9, machine.Pin.OUT)
-segment_2 = machine.Pin(10, machine.Pin.OUT)
-segment_3 = machine.Pin(11, machine.Pin.OUT)
-segment_4 = machine.Pin(12, machine.Pin.OUT)
-segment_5 = machine.Pin(13, machine.Pin.OUT)
-segment_6 = machine.Pin(14, machine.Pin.OUT)
+afficheur_Unitees = [
+    machine.Pin(16, machine.Pin.OUT),
+    machine.Pin(20, machine.Pin.OUT),
+    machine.Pin(21, machine.Pin.OUT),
+    machine.Pin(22, machine.Pin.OUT),
+    machine.Pin(26, machine.Pin.OUT),
+    machine.Pin(27, machine.Pin.OUT),
+    machine.Pin(28, machine.Pin.OUT)
+]
 
-afficheur_Dixaines = [segment_0, segment_1, segment_2,
-                      segment_3, segment_4, segment_5, segment_6]
-
-segment_0 = machine.Pin(16, machine.Pin.OUT)
-segment_1 = machine.Pin(20, machine.Pin.OUT)
-segment_2 = machine.Pin(21, machine.Pin.OUT)
-segment_3 = machine.Pin(22, machine.Pin.OUT)
-segment_4 = machine.Pin(26, machine.Pin.OUT)
-segment_5 = machine.Pin(27, machine.Pin.OUT)
-segment_6 = machine.Pin(28, machine.Pin.OUT)
-
-afficheur_Unitees = [segment_0, segment_1, segment_2,
-                     segment_3, segment_4, segment_5, segment_6]
-
-
-# Définition du la broche pour le point
+# Définition de la broche pour le point
 dot_point = machine.Pin(5, machine.Pin.OUT)
 
 # Définition des broches pour les leds
@@ -92,13 +83,11 @@ echo = machine.Pin(19, machine.Pin.IN)
 # Fonction pour afficher un chiffre sur un afficheur 7 segments
 def afficher_chiffre(digit, segments):
     digit = int(digit)
-
     for i in range(len(segments)):
         if digit_patterns[digit][i]:
             segments[i].on()
         else:
             segments[i].off()
-
 
 # Boucle principale pour mesurer la distance et afficher les deux premiers chiffres
 while True:
@@ -112,7 +101,7 @@ while True:
         green_led.on()
 
 
-    segment_0.on()
+    machine.Pin(8, machine.Pin.OUT).on()
     if distance > 99:
         dot_point.off()
 
